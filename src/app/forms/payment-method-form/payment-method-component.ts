c8971bbc25e8fda8319aa4c5e2a8da707205b084
Robert Gifford <timeonator@gmail.com>
2017-04-30 11:11:10 -0700

    Fixed a references to the @angular/forms module

--- src/app/forms/payment-method-form/payment-method-component.ts
+++ src/app/forms/payment-method-form/payment-method-component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { PaymentMethod }    from '../../shared/models/paymentmethod';
 
